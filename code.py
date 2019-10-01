daf03500059223e9954c5d7f6e1ac1d3b4529b99
06keito <keito.yamamoto06@gmail.com>
2019-10-01 15:25:27 +0900

    rm ignore files

--- code.py
+++ code.py
@@ -5,7 +5,7 @@ f = open('input.txt', 'r')
 sys.stdin = f
 #--------------------------
 #--------------------------
-filename = 'test.py'
+filename = 'test.py'#filename
 out = open(filename,"w")
 shutil.copy("code.py",filename)
 out.close()
